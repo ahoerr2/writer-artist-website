51592b1458a32e7d0aef07af6b83905c19e8f325
ahoerr2 <42220471+ahoerr2@users.noreply.github.com>
2023-04-25 19:53:32 -0500

    Added SEO support

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,6 @@
 import NavBar from "@/components/NavBar";
 import Head from "next/head";
+import { NextSeo } from "next-seo";
 
 export default function Layout({ title, children }) {
   return (
@@ -10,6 +11,21 @@ export default function Layout({ title, children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <NextSeo
+        title={"Writer Artist Portfolio Site: " + (title ? title : "Untitled")}
+        description="This is a demo for a writer artist portfolio site. Will have integration with wordpress and other popular utils to make site upkeep effortless."
+        openGraph={{
+          title: "Writer Artist Portfolio Site" + (title ? title : "Untitled"),
+          description:
+            "This is a demo for a writer artist portfolio site. Will have integration with wordpress and other popular utils to make site upkeep effortless.",
+          images: [
+            {
+              url: "/seo-icon.png",
+              alt: "My image alt text",
+            },
+          ],
+        }}
+      />
       <NavBar />
       <main className="body-connect container">{children}</main>
     </>
